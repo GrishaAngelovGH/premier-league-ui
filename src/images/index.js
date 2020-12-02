b290d7962b674f5885036734dc3c987ed33d0e9c
Grisha Angelov <grisha.angelov89@gmail.com>
2020-12-02 10:56:14 +0200

    Add remaining images of football clubs

--- src/images/index.js
+++ src/images/index.js
@@ -1,17 +1,25 @@
 import premierLeagueLogo from './pl-logo.png'
 
+import arsenal from './arsenal.png'
+import astonVilla from './aston-villa.png'
 import brighton from './brighton.png'
 import burnley from './burnley.png'
+import chelsea from './chelsea.png'
 import crystalPalace from './crystal-palace.png'
 import everton from './everton.png'
+import fulham from './fulham.png'
 import leeds from './leeds.png'
+import leicester from './leicester.png'
 import liverpool from './liverpool.png'
 import manCity from './man-city.png'
 import manUtd from './man-utd.png'
 import newcastle from './newcastle.png'
 import sheffield from './sheffield.png'
 import southampton from './southampton.png'
+import tottenham from './tottenham.png'
 import westBrom from './west-brom.png'
+import westHam from './west-ham.png'
+import wolves from './wolves.png'
 
 import news1 from './news1.jpeg'
 import news2 from './news2.jpeg'
@@ -24,16 +32,24 @@ export const news = {
 
 export default {
     premierLeagueLogo,
+    arsenal,
+    astonVilla,
     brighton,
     burnley,
+    chelsea,
     crystalPalace,
     everton,
+    fulham,
     leeds,
+    leicester,
     liverpool,
     manCity,
     manUtd,
     newcastle,
     sheffield,
     southampton,
-    westBrom
+    tottenham,
+    westBrom,
+    westHam,
+    wolves
 }
