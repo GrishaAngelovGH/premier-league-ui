9a03ff9ed3e408404e21326aa90b92e7ba1c9225
Grisha Angelov <grisha.angelov89@gmail.com>
2023-04-10 13:58:56 +0300

    Improve players image visualization in Players component

--- src/components/Pages/ContentPage/PageBody/Players/Players.js
+++ src/components/Pages/ContentPage/PageBody/Players/Players.js
@@ -4,7 +4,7 @@ import './Players.scss'
 const Players = () => (
     <div className='row no-gutters vh-100 players-bg'>
         <div className='col-md-12'>
-            <img src={players} className='img-fluid' />
+            <img src={players} className='w-100' />
         </div>
     </div>
 )
