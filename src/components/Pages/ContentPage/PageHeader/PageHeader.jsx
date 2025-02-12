777c04924b2dec7ea8d139037d0356724d6a2769
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-12 14:21:36 +0200

    Migrate ContentPage/PageHeader component to Tailwind CSS

--- src/components/Pages/ContentPage/PageHeader/PageHeader.jsx
+++ src/components/Pages/ContentPage/PageHeader/PageHeader.jsx
@@ -1,37 +1,36 @@
 import { Link } from 'react-router-dom'
 
 import Menu from './Menu'
-import './PageHeader.scss'
 
 export const ArrowLeft = () => (
-    <svg width='2em' height='2em' viewBox='0 0 16 16' className='bi bi-arrow-left-short' fill='white' xmlns='http://www.w3.org/2000/svg'>
-        <path fillRule='evenodd' d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z' />
-    </svg>
+	<Link to='/'>
+		<svg width='2em' height='2em' viewBox='0 0 16 16' className='bi bi-arrow-left-short' fill='white' xmlns='http://www.w3.org/2000/svg'>
+			<path fillRule='evenodd' d='M12 8a.5.5 0 0 1-.5.5H5.707l2.147 2.146a.5.5 0 0 1-.708.708l-3-3a.5.5 0 0 1 0-.708l3-3a.5.5 0 1 1 .708.708L5.707 7.5H11.5a.5.5 0 0 1 .5.5z' />
+		</svg>
+	</Link>
 )
 
 export const CloseBtn = () => (
-    <Link to='/'>
-        <span role='button'>
-            <svg width='2em' height='2em' viewBox='0 0 16 16' className='bi bi-x' fill='white' xmlns='http://www.w3.org/2000/svg'>
-                <path fillRule='evenodd' d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z' />
-            </svg>
-        </span>
-    </Link>
+	<Link to='/'>
+		<span role='button'>
+			<svg width='2em' height='2em' viewBox='0 0 16 16' className='bi bi-x' fill='white' xmlns='http://www.w3.org/2000/svg'>
+				<path fillRule='evenodd' d='M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z' />
+			</svg>
+		</span>
+	</Link>
 )
 
 const PageHeader = () => (
-    <div className='row flex-nowrap overflow-auto no-gutters content-header'>
-        <div className='d-flex'>
-            <Link to='/'>
-                <ArrowLeft />
-            </Link>
-            <div className='m-1'>Premier League</div>
-        </div>
+	<div className='flex justify-between items-center bg-purple-900 text-white'>
+		<div className='flex items-center p-4'>
+			<ArrowLeft />
+			<div>Premier League</div>
+		</div>
 
-        <Menu />
+		<Menu />
 
-        <CloseBtn />
-    </div>
+		<CloseBtn />
+	</div>
 )
 
 export default PageHeader
