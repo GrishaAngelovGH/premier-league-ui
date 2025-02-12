e0ed8746b2f58287f3224449d9a901eca98880fc
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-12 15:18:41 +0200

    Migrate ContentPage/PageBody/News component to Tailwind CSS

--- src/components/Pages/ContentPage/PageBody/News/News.jsx
+++ src/components/Pages/ContentPage/PageBody/News/News.jsx
@@ -1,60 +1,51 @@
-import PropTypes from 'prop-types'
-
 import { news } from 'images'
 
 export const newsItems = [
-    {
-        title: 'Guardiola rules out new Manchester City signings in January',
-        source: 'Goal.com',
-        time: '4 days ago',
-        image: news['news1']
-    },
-    {
-        title: `'Welbeck said it wasn't a penalty!' – Henderson fumes at VAR after 
+	{
+		title: 'Guardiola rules out new Manchester City signings in January',
+		source: 'Goal.com',
+		time: '4 days ago',
+		image: news['news1']
+	},
+	{
+		title: `'Welbeck said it wasn't a penalty!' – Henderson fumes at VAR after 
         Liverpool are held by Brighton`,
-        source: 'Goal.com',
-        time: '3 days ago',
-        image: news['news2']
-    },
-    {
-        title: 'Chelsea 0-0 Tottenham Hotspur: Spurs return to the top of the Premier League',
-        source: 'BBC News',
-        time: '2 days ago',
-        image: news['news3']
-    },
-    {
-        title: 'Return of supporters to Stamford Bridge | Official Site | Chelsea Football Club',
-        source: 'Chelsea FC',
-        time: '5 days ago',
-        image: news['news4']
-    }
+		source: 'Goal.com',
+		time: '3 days ago',
+		image: news['news2']
+	},
+	{
+		title: 'Chelsea 0-0 Tottenham Hotspur: Spurs return to the top of the Premier League',
+		source: 'BBC News',
+		time: '2 days ago',
+		image: news['news3']
+	},
+	{
+		title: 'Return of supporters to Stamford Bridge | Official Site | Chelsea Football Club',
+		source: 'Chelsea FC',
+		time: '5 days ago',
+		image: news['news4']
+	}
 ]
 
 const News = ({ newsItems }) => (
-    <div className='row no-gutters justify-content-center mt-3'>
-        <div className='col-md-9 col-lg-6'>
-            {
-                newsItems.map(v => (
-                    <div className='row border rounded p-2' key={v.title}>
-                        <div className='col-md-9'>
-                            <a href='#/content/news'>{v.title}</a>
-                            <div className='text-secondary small'>
-                                {v.source} {v.time}
-                            </div>
-                        </div>
-                        <div className='col-md-2'>
-                            <img src={v.image} className='rounded' />
-                        </div>
-                    </div>
-                ))
-            }
-
-        </div>
-    </div>
+	<div className='flex flex-col gap-5 items-center mt-3'>
+		{
+			newsItems.map(v => (
+				<div className='flex items-center border-2 border-gray-400 rounded-md text-center p-1 w-1/2' key={v.title}>
+					<div className='w-4/6 md:w-5/6'>
+						<a href='#/content/news'>{v.title}</a>
+						<div className='text-gray-500 text-sm'>
+							{v.source} {v.time}
+						</div>
+					</div>
+					<div className='w-2/6 md:w-1/6'>
+						<img src={v.image} className='rounded-md' />
+					</div>
+				</div>
+			))
+		}
+	</div>
 )
-
-News.propTypes = {
-    newsItems: PropTypes.array.isRequired
-}
 
 export default News
--- src/components/Pages/ContentPage/PageBody/News/News.test.jsx
+++ src/components/Pages/ContentPage/PageBody/News/News.test.jsx
@@ -3,16 +3,16 @@ import { render } from '@testing-library/react'
 import News from './News'
 
 test('should render News component', () => {
-    const newsItems = [
-        {
-            title: 'Guardiola rules out new Manchester City signings in January',
-            source: 'Goal.com',
-            time: '4 days ago',
-            image: 'news1'
-        }
-    ]
+	const newsItems = [
+		{
+			title: 'Guardiola rules out new Manchester City signings in January',
+			source: 'Goal.com',
+			time: '4 days ago',
+			image: 'news1'
+		}
+	]
 
-    const view = render(<News newsItems={newsItems} />)
+	const view = render(<News newsItems={newsItems} />)
 
-    expect(view).toMatchSnapshot()
+	expect(view).toMatchSnapshot()
 })
