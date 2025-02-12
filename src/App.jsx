fa9aed89bfdcd9f14e435fcf96c5a49ce3d2efc0
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-12 10:38:34 +0200

    Use standard BrowserRouter and remove exact prop from the home page route, because for static paths is enabled by default

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { HashRouter as Router, Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { HomePage, ContentPage } from 'components/Pages'
 
 function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path='/' element={<HomePage />} />
+        <Route path='/' element={<HomePage />} />
         <Route path='/content/*' element={<ContentPage />} />
       </Routes>
     </Router>
--- src/App.test.jsx
+++ src/App.test.jsx
@@ -3,7 +3,7 @@ import { render } from '@testing-library/react'
 import App from './App'
 
 test('should render component', () => {
-    const view = render(<App />)
+	const view = render(<App />)
 
-    expect(view).toMatchSnapshot()
+	expect(view).toMatchSnapshot()
 })
