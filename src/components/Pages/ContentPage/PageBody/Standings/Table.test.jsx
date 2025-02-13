adf7ad66573507f6768261cda8ef45a42d2d2624
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-13 10:10:44 +0200

    Migrate ContentPage/PageBody/Standings/Table component to Tailwind CSS

--- src/components/Pages/ContentPage/PageBody/Standings/Table.test.jsx
+++ src/components/Pages/ContentPage/PageBody/Standings/Table.test.jsx
@@ -3,27 +3,27 @@ import { render } from '@testing-library/react'
 import Table from './Table'
 
 test('should render Table component', () => {
-    const view = render(
-        <Table
-            rows={[
-                {
-                    position: 1,
-                    image: 'tottenham-image',
-                    title: 'Tottenham',
-                    mp: 10,
-                    w: 6,
-                    d: 3,
-                    l: 1,
-                    gf: 21,
-                    ga: 9,
-                    gd: 12,
-                    pts: 21,
-                    lastFive: ['d', 'w', 'w', 'w', 'w']
-                }
-            ]}
-            columns={['Club', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts', 'Last 5']}
-        />
-    )
+	const view = render(
+		<Table
+			rows={[
+				{
+					position: 1,
+					image: 'tottenham-image',
+					title: 'Tottenham',
+					mp: 10,
+					w: 6,
+					d: 3,
+					l: 1,
+					gf: 21,
+					ga: 9,
+					gd: 12,
+					pts: 21,
+					lastFive: ['d', 'w', 'w', 'w', 'w']
+				}
+			]}
+			columns={['Club', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts', 'Last 5']}
+		/>
+	)
 
-    expect(view).toMatchSnapshot()
+	expect(view).toMatchSnapshot()
 })
