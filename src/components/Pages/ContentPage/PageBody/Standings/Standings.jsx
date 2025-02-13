9a0a4f21099246aad930d14fdfac4ded47f9142e
Grisha Angelov <grisha.angelov89@gmail.com>
2025-02-13 09:41:28 +0200

    Migrate ContentPage/PageBody/Standings component to Tailwind CSS

--- src/components/Pages/ContentPage/PageBody/Standings/Standings.jsx
+++ src/components/Pages/ContentPage/PageBody/Standings/Standings.jsx
@@ -1,24 +1,18 @@
-import PropTypes from 'prop-types'
-
 import Table from './Table'
 
 const Standings = ({ standingItems }) => (
-    <div className='row no-gutters justify-content-center mt-3'>
-        <div className='col-md-9 col-lg-6'>
-            <div className='bg-light p-2 text-center'>
-                Current Season
-            </div>
+	<div className='flex justify-center mt-3'>
+		<div className='w-1/2'>
+			<div className='bg-gray-200 text-xl p-2 text-center rounded-md'>
+				Current Season
+			</div>
 
-            <Table
-                rows={standingItems}
-                columns={['Club', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts', 'Last 5']}
-            />
-        </div>
-    </div >
+			<Table
+				rows={standingItems}
+				columns={['Club', 'MP', 'W', 'D', 'L', 'GF', 'GA', 'GD', 'Pts', 'Last 5']}
+			/>
+		</div>
+	</div >
 )
-
-Standings.propTypes = {
-    standingItems: PropTypes.array.isRequired
-}
 
 export default Standings
--- src/components/Pages/ContentPage/PageBody/Standings/Standings.test.jsx
+++ src/components/Pages/ContentPage/PageBody/Standings/Standings.test.jsx
@@ -3,24 +3,24 @@ import { render } from '@testing-library/react'
 import Standings from './Standings'
 
 test('should render Standings component', () => {
-    const standingItems = [
-        {
-            position: 1,
-            image: 'tottenham-image',
-            title: 'Tottenham',
-            mp: 10,
-            w: 6,
-            d: 3,
-            l: 1,
-            gf: 21,
-            ga: 9,
-            gd: 12,
-            pts: 21,
-            lastFive: ['d', 'w', 'w', 'w', 'w']
-        }
-    ]
+	const standingItems = [
+		{
+			position: 1,
+			image: 'tottenham-image',
+			title: 'Tottenham',
+			mp: 10,
+			w: 6,
+			d: 3,
+			l: 1,
+			gf: 21,
+			ga: 9,
+			gd: 12,
+			pts: 21,
+			lastFive: ['d', 'w', 'w', 'w', 'w']
+		}
+	]
 
-    const view = render(<Standings standingItems={standingItems} />)
+	const view = render(<Standings standingItems={standingItems} />)
 
-    expect(view).toMatchSnapshot()
+	expect(view).toMatchSnapshot()
 })
